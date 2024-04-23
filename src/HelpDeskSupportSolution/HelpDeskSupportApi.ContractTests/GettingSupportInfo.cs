82d07d8024a591f78037a16e67875f7d211694d4
Abria Marshall <abria.marshall@gmail.com>
2024-04-23 12:39:15 -0400

    before lunch

---
+++ src/HelpDeskSupportSolution/HelpDeskSupportApi.ContractTests/GettingSupportInfo.cs
@@ -0,0 +1,24 @@
+﻿using Alba;
+
+namespace HelpDeskSupportApi.ContractTests;
+public class GettingSupportInfo
+{
+    [Fact]
+    public async Task WhenWeAreOpen()
+    {
+        var expectedResults = new SupportResponseModel("Joe Smith", "555-1212", "joe@company.com");
+        var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/"); // Do a get request to the api to this ("/") resource.
+            api.StatusCodeShouldBeOk();
+        });
+
+        var actualResponse = await response.ReadAsJsonAsync<SupportResponseModel>();
+
+        Assert.NotNull(actualResponse);
+
+        Assert.Equal(expectedResults, actualResponse);
+    }
+}
