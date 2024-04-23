920d90da00eb1a06e9d584732c88da7abf961617
Abria Marshall <abria.marshall@gmail.com>
2024-04-23 16:42:17 -0400

    end tuesday

--- src/help-desk/IssueTrackerSolution/IssueTrackerApi/Program.cs
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Program.cs
@@ -1,5 +1,7 @@
 using FluentValidation;
+using IssueTrackerApi;
 using IssueTrackerApi.Controllers.Issues;
+using IssueTrackerApi.Services;
 using Marten;
 using System.Text.Json.Serialization;
 
@@ -12,6 +14,7 @@ builder.Services.AddMarten(options =>
     options.Connection(databaseConnectionString);
 }).UseLightweightSessions();
 
+builder.Services.AddValidatorsFromAssemblyContaining<CreateIssueRequestModelValidator>();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
@@ -19,8 +22,13 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateIssueRequestModelValidator>();
 
+var apiUrl = builder.Configuration.GetValue<string>("api-url") ?? throw new Exception("Need the Support Api Url");
+builder.Services.AddHttpClient<SupportHttpClient>(client =>
+{
+    client.BaseAddress = new Uri(apiUrl);
+}).AddPolicyHandler(BasicSrePolicies.GetDefaultRetryPolicy())
+    .AddPolicyHandler(BasicSrePolicies.GetDefaultCircuitBreaker());
 // stuff above this line - configuration of api and all the stuff inside of it
 var app = builder.Build();
 //stuff after this line - how it takes requests and makes them into responses
